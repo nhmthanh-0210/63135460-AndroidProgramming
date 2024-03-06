d9807e0ef55210fe9e37e66a3db925aa7848b2be
nhmthanh-0210 <thanhnguyenminhho2003@gmail.com>
2024-03-06 14:48:24 +0700

    create BTN

---
+++ app/src/main/java/com/example/vd1/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.vd1;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.myapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);//Nap layout vao
+
+
+
+    }
+}
