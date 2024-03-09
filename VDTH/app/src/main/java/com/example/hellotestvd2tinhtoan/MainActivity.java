6501faeefb5068dc885c3605d72e68a441cb4646
nhmthanh-0210 <thanhnguyenminhho2003@gmail.com>
2024-03-09 11:46:16 +0700

    Done tính toán

--- VDTH/app/src/main/java/com/example/hellotestvd2tinhtoan/MainActivity.java
+++ VDTH/app/src/main/java/com/example/hellotestvd2tinhtoan/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.hellotestvd2tinhtoan;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,28 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void XuLiCong(View view){
+        // tìm, tham chiếu đến các điều khiển trên tập tin xml
+         EditText editTextSoA =  findViewById(R.id.edta);
+         EditText   editTextSoB = findViewById(R.id.edtb);
+         EditText editTextKetQua = findViewById(R.id.editTextText3);
+
+         // lấy dữ liệu về dungf phương thức getText từ số a
+         String strA =   editTextSoA.getText().toString();
+         //lấy từ số b
+        String strB = editTextSoB.getText().toString();
+
+        // chuyển dữ liệu sang dạng số
+       int so_A = Integer.parseInt(strA);
+        int so_B = Integer.parseInt(strB);
+    // tính toán theo yêu cầu
+        int Tong = so_A+ so_B;
+    String strTong = String.valueOf(Tong);// chuyển sang dạng chuỗi
+        // hiện ra màn hình
+        editTextKetQua.setText(strTong);
+
+
     }
 }
