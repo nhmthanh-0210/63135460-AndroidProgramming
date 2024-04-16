3b20998bd5087065a68d703a1f2982092414a015
nhmthanh-0210 <thanhnguyenminhho2003@gmail.com>
2024-04-16 07:53:56 +0700

    create item

--- RecylerViewPage2/app/src/main/java/ntu/nguyenhominhthanh_63135460/recylerviewpage2/MainActivity.java
+++ RecylerViewPage2/app/src/main/java/ntu/nguyenhominhthanh_63135460/recylerviewpage2/MainActivity.java
@@ -7,8 +7,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    LandScapeAdapter landScapeAdapter;
+    ArrayList<LandScape> ViewPagerData;
+
+    ViewPager2 viewPager2Land;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,39 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        ViewPagerData = getDataForViewPager();
+        viewPager2Land = findViewById(R.id.VP2Land);
+
+        landScapeAdapter = new LandScapeAdapter(this, ViewPagerData);
+        viewPager2Land.setAdapter(landScapeAdapter);
+
+        viewPager2Land.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                super.onPageScrollStateChanged(state);
+            }
+        });
     }
-}
+    ArrayList<LandScape> getDataForViewPager() {
+        ArrayList<LandScape> dsDuLieu = new ArrayList<LandScape>();
+        dsDuLieu.add(new LandScape("anh1", "View dep"));
+        dsDuLieu.add(new LandScape("anh2", "View dep"));
+        dsDuLieu.add(new LandScape("anh3", "View dep"));
+        dsDuLieu.add(new LandScape("anh4", "View dep"));
+
+        return dsDuLieu;
+    }
+
+    }
