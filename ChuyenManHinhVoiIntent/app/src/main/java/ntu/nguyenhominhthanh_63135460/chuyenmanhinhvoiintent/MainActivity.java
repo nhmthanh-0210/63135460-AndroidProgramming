701683acdd68be7f44342dde0947e3dd0f1c9214
nhmthanh-0210 <thanhnguyenminhho2003@gmail.com>
2024-05-12 23:51:18 +0700

    complete Intent

--- ChuyenManHinhVoiIntent/app/src/main/java/ntu/nguyenhominhthanh_63135460/chuyenmanhinhvoiintent/MainActivity.java
+++ ChuyenManHinhVoiIntent/app/src/main/java/ntu/nguyenhominhthanh_63135460/chuyenmanhinhvoiintent/MainActivity.java
@@ -1,8 +1,10 @@
 package ntu.nguyenhominhthanh_63135460.chuyenmanhinhvoiintent;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,12 +14,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.edtuoiiiii), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
@@ -33,6 +36,22 @@ public class MainActivity extends AppCompatActivity {
 
         //phai co 2 tham so, tham so 2 la man hinh chuyen toi(.class)
         thuKichHoatMH2 = new Intent(MainActivity.this, MainActivity2.class);
+
+
+        //1+ gói dữ liệu vào
+
+        EditText edtTen = findViewById(R.id.edthovaten);
+        EditText edTuoi = findViewById(R.id.edttuoi);
+
+        //1++2lấy dữ liệu từ điều khiển tương ứng
+        String strTen = edtTen.getText().toString();
+        String strTuoi = edTuoi.getText().toString();
+//        int tuoi = Integer.parseInt(strTuoi);
+
+        //1.3 gói vào thư, mỗi dữ liệu ta cho một cái key để bên kia dựa vào đó để bóc ra
+
+        thuKichHoatMH2.putExtra("tete" , strTen);
+        thuKichHoatMH2.putExtra("teteso" , strTuoi);
 
 
         //gui thu ma khong doi phan hoi
--- ChuyenManHinhVoiIntent/app/src/main/java/ntu/nguyenhominhthanh_63135460/chuyenmanhinhvoiintent/MainActivity2.java
+++ ChuyenManHinhVoiIntent/app/src/main/java/ntu/nguyenhominhthanh_63135460/chuyenmanhinhvoiintent/MainActivity2.java
@@ -1,9 +1,11 @@
 package ntu.nguyenhominhthanh_63135460.chuyenmanhinhvoiintent;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +15,16 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.edtuoiiiii), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
@@ -38,6 +44,20 @@ public class MainActivity2 extends AppCompatActivity {
         btnManHinh2.setOnClickListener(BoLangNgheChuyenMH);
         //code ngay duoi
 
+
+
+        //khi màn hình được mở ra ta lấy về intent đã kích hoạt
+        //bóc dữ liệu ra xem
+        Intent thunhanduc = getIntent();
+        String ten = thunhanduc.getStringExtra("tete");
+        String tuoi = thunhanduc.getStringExtra("teteso");
+
+//đưa lên điêuf khiển
+        TextView tvTen = findViewById(R.id.tvHoTen);
+        TextView tvTuoi = findViewById(R.id.tvTuoi);
+
+        tvTen.setText("Họ và tên:" + ten);
+        tvTuoi.setText("Tuổi:" + tuoi);
     }
 
     //code o day
@@ -48,11 +68,9 @@ public class MainActivity2 extends AppCompatActivity {
         //xu li cu the o day
             //1 tao thu
             Intent thuKichHoatMH1 = new Intent(MainActivity2.this, MainActivity.class);
-
             //khong doi phan hoi
             startActivity(thuKichHoatMH1);
         }
     };
-
 
 }
